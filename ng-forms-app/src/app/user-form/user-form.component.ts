ee36f23b772f27696ce4c72aba2688c88fc92f53
sbtalk71 <sbtalk@yahoo.com>
2025-07-30 17:58:18 +0530

    forms app uploaded

---
+++ ng-forms-app/src/app/user-form/user-form.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.css'
+})
+export class UserFormComponent {
+  userForm!: FormGroup;
+  constructor( private fb:FormBuilder){
+   this.initForm();
+  }
+/*
+  initForm(){
+    this.userForm=new FormGroup(
+      {
+        username:new FormControl(''),
+        email:new FormControl(''),
+        age:new FormControl(''),
+        phone:new FormControl('')
+      }
+    );
+  }
+    */
+   initForm(){
+    this.userForm=this.fb.group({
+      username:['',[Validators.required,Validators.minLength(4)]],
+      email:['',[Validators.required,Validators.email]],
+      age: ['',[Validators.required,Validators.pattern('/^1[0-9]|[2-9][0-9]$/')]],
+      phone: ['',[Validators.required]]
+    });
+   }
+}
